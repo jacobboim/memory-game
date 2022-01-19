1133ab90b2c497367fdfb0f62a01aedafea2cf40
jacobboim <57962931+jacobboim@users.noreply.github.com>
2022-01-19 13:39:51 -0500

    added different pics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png", matched: false },
-  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/NoamElimelech.jpeg", matched: false },
+  { src: "/img/kedusha.jpeg", matched: false },
   { src: "/img/ring-1.png", matched: false },
   { src: "/img/scroll-1.png", matched: false },
   { src: "/img/shield-1.png", matched: false },
